40d19a7027f61f45d09fce789d2b71c7b4cc9b12
Arktische <yuxingtu@outlook.com>
2021-12-23 22:18:23 -0800

    add fast clock

---
+++ common/defer.hpp
@@ -0,0 +1,29 @@
+//
+// Created by tyx on 12/23/21.
+//
+
+#ifndef TINYRPC_DEFER_HPP
+#define TINYRPC_DEFER_HPP
+#include <functional>
+namespace common {
+#define CONCAT_(a, b) a ## b
+#define CONCAT(a, b) CONCAT_(a,b)
+#define DEFER(fn) Defer CONCAT(__defer__, __LINE__) = [&] ( ) { fn ; }
+    class Defer {
+    public:
+        template<class Func>
+        Defer(Func &&fn) : fn_(std::forward<Func>(fn)){}
+        Defer(Defer &&other) noexcept : fn_(std::move(other.fn_))  {
+            other.fn_ = nullptr;
+        }
+        ~Defer() {
+            if(fn_) fn_();
+        }
+
+        Defer(const Defer &) = delete;
+        void operator=(const Defer &) = delete;
+    private:
+        std::function<void()> fn_;
+    };
+}
+#endif //TINYRPC_DEFER_HPP
