cd386b4e1379cb0314b52514085ea6f790737526
Arktische <yuxingtu@outlook.com>
2022-01-25 18:39:19 +0800

    feat: add fast memcpy with SSE
    Change-Id: Ic21e71ab85fb904d85a12780bfff589ebdfaa77c

--- common/log.hpp
+++ common/log.hpp
@@ -21,14 +21,11 @@ enum LogLevel { DEBUG, INFO, WARN, ERROR, FATAL };
 std::vector<std::string> kLogLevelStr = {"[DEBUG]", "[INFO]", "[WARN]",
                                          "[ERROR]", "[FATAL]"};
 namespace common {
-// LogMessageData
-
 using common::NonCopyable;
 using std::cout;
 using std::ostream;
 using std::size_t;
 using std::streambuf;
-static const int kMaxNumericSize = 48;
 static const int kSmallBufferSize = 1024;
 static const int kLargeBufferSize = 4096;
 
@@ -40,22 +37,18 @@ inline void memcpy(char* dst, const char* src, size_t size) {
   }
   auto p = reinterpret_cast<const char*>((reinterpret_cast<size_t>(src) + 31) &
                                          (~31ll));
-
   auto diff = p - src;
   std::memcpy(dst, src, diff);
   dst += diff;
   src += diff;
   size -= diff;
-
   size_t tms = size >> 5;
-
   while (tms--) {
     __m256i _src = _mm256_load_si256(reinterpret_cast<const __m256i*>(src));
     _mm256_store_si256(reinterpret_cast<__m256i*>(dst), _src);
     dst += 16;
     src += 16;
   }
-
   std::memcpy(dst, src, size & 0x1f);
 }
 #endif
